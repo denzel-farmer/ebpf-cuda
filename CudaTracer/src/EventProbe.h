bacb4776c71c2b7c7238cf1b87f0794458595d56
Denzel Farmer <denzelgfarmer@gmail.com>
2024-12-05 03:31:30 +0000

    Add blank eventprobe files

---
+++ CudaTracer/src/EventProbe.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+// Abstract class representing one probe that will be ijected into proccess 
+class EventProbe {
+    
+};
+
+
+
+// Implementationf of an eBPF probe
+class BPFProbe : public EventProbe {
+    
+};
