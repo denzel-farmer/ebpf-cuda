58ce241fbb07b199dc612c1b445e5ea6ec3645bd
Denzel Farmer <denzelgfarmer@gmail.com>
2024-12-06 00:57:48 +0000

    Create test for traceragent

--- CudaTracer/test/TestTracerAgent.cpp
+++ CudaTracer/test/TestTracerAgent.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <csignal>
+#include <unistd.h>
 
-int main() {
-    std::cout << "Placeholder main for TestTracerAgent" << std::endl;
+#include "TracerAgent.h"
+
+void signalHandler(int signum) {
+    std::cout << "Interrupt signal (" << signum << ") received.\n";
+    exit(signum);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <pid>" << std::endl;
+        return 1;
+    }
+
+    int pid = std::stoi(argv[1]);
+
+
+    // Register signal handler
+    signal(SIGINT, signalHandler);
+
+    TracerAgent agent(pid);
+
+    std::cout << "TracerAgent created, press enter to start" << std::endl;
+
+    std::cin.get();
+
+    agent.StartAgentAsync();
+
+    std::cout << "Agent started. Press Enter to dump history and exit..." << std::endl;
+    std::cin.get();
+
+    agent.dumpHistory("history_dump.json");
+    std::cout << "History dumped to history_dump.json" << std::endl;
+
     return 0;
 }
